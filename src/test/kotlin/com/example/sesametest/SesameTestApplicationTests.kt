c254d00ad63569c4efdd1df0dd18b2b18f92aa5d
Bella (박소원) <psw@gsitm.com>
2022-09-26 13:32:47 +0900

    dep

--- src/test/kotlin/com/example/sesametest/SesameTestApplicationTests.kt
+++ src/test/kotlin/com/example/sesametest/SesameTestApplicationTests.kt
@@ -1,13 +1,11 @@
 package com.example.sesametest
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class SesameTestApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class SesameTestApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
